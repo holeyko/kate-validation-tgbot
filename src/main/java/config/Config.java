4c76cc84cd40c4310855319f37ef64b7a57bc3de
Vadim Ryabov <mr.holeyko@gmail.com>
2022-09-29 16:37:34 +0300

    added Config.java

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,7 @@
+package config;
+
+public interface Config {
+    public static final String BOT_NAME = "Kate Validation Bot";
+    public Static final String KATE_CHANNEL = "k_visual";
+    public static final String LINK_AFTER_VALIDATE = "https://google.com";
+}
